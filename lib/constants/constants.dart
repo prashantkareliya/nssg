8742d052ac6fb8075ea4d796e4b0698958e7b0da
mahendra-iihglobal <73980629+mahendra-iihglobal@users.noreply.github.com>
2023-05-24 14:50:34 +0530

    bug fix

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -20,8 +20,8 @@ class Constants {
 
   factory Constants._prd() {
     return const Constants(
-        //  endpoint: 'http://122.170.107.160/nssg/',
-        endpoint: 'https://nssgapp.co.uk/',
+        endpoint: 'http://122.170.107.160/nssg/',
+        // endpoint: 'https://nssgapp.co.uk/',
         //Base URl
         apiKeyContact:
             'VmxSQ1YxUldTbFpVYkZaS1UwVTFiMWxyYUZkTlJteFpWVzVDYVUxcVZYZGFWbWhEWWtWNFNGZHVRbXBpYXpSM1dXMHhSMlJHY0ZSbFIzQnBUV3BWZDFkV1pFOU5SbWQ1VGxoYVRWTkZTblpaYWtreFlrVjRTR1ZIYUdwTk1Vb3hWMVpqZUdKRmVFaE5XRnBhWWxkNGVsZHNUalJpUjBwWVVtNUNhVkV6YUhGWmFra3hUVVpzV0ZScVFsbE5helV5V1d4b1EyRkhTblZoTTA1cFZqQmFkMWxyWkhOa1ZtOTZWR3BDYW1KV1duTmFSVTQwWkVac1dHSklUbWhXZWxaMVYxUktjMDFIVmxSbFNGSmFWako0ZWxsV1l6RmliR3Q1VDFSR2FXSnNTalZhVms0MFpFWnNXR0pJVG1oV2VsWjFXbGN4YzJRd2VFaFBWRUpvVWpGYU5WbDZUbE5sVm5CWVZtcENUVko2YTNkWlZXUlhaVlpyZVdKRVFteFZNMmd5V2tWa2IySkhUblJVYmxwclZucFZkMWt5TlhKak1rbDZWVzA1WVZkRmJ6SlpWbWhDV2pGS2MxTnNRbFZWTUVwRldXcEpNVTFHYkZoVWFrSnFaVzVOT1E9PQ==',
